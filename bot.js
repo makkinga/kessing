784eae6f94c6f1561096a5da1b0261895e3404f0
Gydo Makkinga <gydo@makkinga.online>
2022-03-10 16:12:47 +0100

    Temporarily disabled message counting

--- bot.js
+++ bot.js
@@ -23,34 +23,34 @@ for (const file of commandFiles) {
     client.commands.set(command.data.name, command)
 }
 
-client.on('messageCreate', async message => {
-    if (!message.author.bot && message.content.split(' ').length >= 3) {
-        const count = await DB.messageCount.findAll({
-            where: {
-                user : message.author.id,
-                guild: message.guildId,
-            },
-            limit: 1,
-        })
-
-        if (count.length === 0) {
-            await DB.messageCount.create({
-                user : message.author.id,
-                guild: message.guildId,
-                count: 1
-            })
-        } else {
-            await DB.messageCount.update({
-                count: count[0].count + 1
-            }, {
-                where: {
-                    user : message.author.id,
-                    guild: message.guildId,
-                }
-            })
-        }
-    }
-})
+// client.on('messageCreate', async message => {
+//     if (!message.author.bot && message.content.split(' ').length >= 3) {
+//         const count = await DB.messageCount.findAll({
+//             where: {
+//                 user : message.author.id,
+//                 guild: message.guildId,
+//             },
+//             limit: 1,
+//         })
+//
+//         if (count.length === 0) {
+//             await DB.messageCount.create({
+//                 user : message.author.id,
+//                 guild: message.guildId,
+//                 count: 1
+//             })
+//         } else {
+//             await DB.messageCount.update({
+//                 count: count[0].count + 1
+//             }, {
+//                 where: {
+//                     user : message.author.id,
+//                     guild: message.guildId,
+//                 }
+//             })
+//         }
+//     }
+// })
 
 client.on('interactionCreate', async interaction => {
     if (!interaction.isCommand()) return
